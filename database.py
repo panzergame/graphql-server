418c17c03ac5e1143b16dde1489b702f18cd348f
tristan <tristan.porteries@gmail.com>
2021-03-17 20:24:11 +0100

    Ajout de configuration

--- database.py
+++ database.py
@@ -2,9 +2,16 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("tp-info802-graphql-server-firebase-adminsdk-snehc-d6f5b0cd40.json")
-firebase_admin.initialize_app(cred)
+db = None
+clients_ref = None
 
-db = firestore.client()
 
-clients_ref = db.collection('clients')
+def init_db(app):
+	global db
+	global clients_ref
+
+	cred = credentials.Certificate(app.config['FIREBASE_CERTIFICATE'])
+	firebase_admin.initialize_app(cred)
+
+	db = firestore.client()
+	clients_ref = db.collection('clients')
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 
+import database
 from schema import schema
 
+import os
+
 app = Flask(__name__)
+app.config.from_pyfile(os.environ.get('GRAPHQL_CONFIG') or 'config/default.cfg')
+
+database.init_db(app)
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
     'graphql',
